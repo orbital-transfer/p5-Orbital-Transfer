5de930dbd88b16c969829950db16ddce61f77b77
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-06-14 18:13:52 -0500

    Fixes for remote handling
    - There can be multiple remotes of the same type; e.g., mutiple push remotes
    
    - Fix for the remote URI extraction regex. It was only using \w which
      breaks for remote names that might have characters such as hyphens ('-').

--- lib/Project/Manager/Repo/Git.pm
+++ lib/Project/Manager/Repo/Git.pm
@@ -10,6 +10,8 @@ has directory => ( is => 'ro', required => 1 );
 
 has _git_wrapper => ( is => 'lazy' ); # _build__git_wrapper
 
+# TODO note that there can be multiple push remotes
+# (and only a single fetch remote? check this)
 has remotes => ( is => 'lazy', clearer => 1 ); # _build_remotes
 
 sub _build__git_wrapper {
@@ -24,12 +26,22 @@ sub _build_remotes {
 
 	my @remotes = $self->_git_wrapper->remote( { verbose => 1 });
 	for my $remote (@remotes) {
-		if( $remote =~ m,(\w+) \s+ (\S+) \s+ \((fetch|push)\)$,x ) {
+		if( $remote =~ m,^(\S+) \s+ (\S+) \s+ \((fetch|push)\)$,x ) {
 			my $remote_name = $1;
 			my $remote_uri = $2;
 			my $remote_type = $3;
 
-			$remote_data{ $remote_name }{$remote_type} = $remote_uri;
+			my $target_ref = \( $remote_data{ $remote_name }{$remote_type} );
+			# TODO Test this.
+			# This allows for multiple (push) refs
+			if( defined $$target_ref ) {
+				unless( ref $$target_ref ) {
+					$$target_ref = [ $$target_ref ];
+				}
+				push @$$target_ref, $remote_uri;
+			} else {
+				$$target_ref = $remote_uri;
+			}
 		} else {
 			die "error parsing remotes for @{[ $self->directory ]}";
 		}
