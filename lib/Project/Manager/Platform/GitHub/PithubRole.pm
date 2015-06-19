092be20ab4f31075bfc2ad701ba5a857f79dd684
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-06-19 01:03:07 -0500

    GitHub: send media type for future GitHub API change preview
    This will be removed later.

--- lib/Project/Manager/Platform/GitHub/PithubRole.pm
+++ lib/Project/Manager/Platform/GitHub/PithubRole.pm
@@ -1,5 +1,8 @@
 package Project::Manager::Platform::GitHub::PithubRole;
 
+use strict;
+use warnings;
+
 use Moo::Role;
 
 use Pithub;
@@ -7,6 +10,22 @@ has _pithub_client => ( is => 'lazy' ); # _build__pithub_client
 
 sub _build__pithub_client {
 	Pithub->new;
+}
+
+has _pithub_options => ( is => 'lazy' ); # _build__pithub_options
+
+sub _build__pithub_options {
+	return +{
+		prepare_request => sub {
+			my ($request) = @_;
+			# add header for GitHub API change preview. Changes take
+			# effect officially June 24.
+			# See:
+			#   - <https://developer.github.com/changes/2015-01-07-prepare-for-organization-permissions-changes/>
+			#   - <https://developer.github.com/changes/2014-12-08-organization-permissions-api-preview/>
+			$request->header( Accept => 'application/vnd.github.moondragon+json; charset=utf-8,application/vnd.github.v3+json; charset=utf-8' );
+		}
+	};
 }
 
 
