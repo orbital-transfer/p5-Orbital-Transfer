ce2ea349b2f77a30a36bbf9cf1ee51bfa01ff06f
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-06-19 06:18:51 -0500

    wip: further setup of Term UI

--- lib/Project/Manager.pm
+++ lib/Project/Manager.pm
@@ -3,6 +3,8 @@ package Project::Manager;
 use strict;
 use warnings;
 
+use Project::Manager::UI::Term;
+
 sub run {
 	my $ui = Project::Manager::UI::Term->new;
 	$ui->run;
--- lib/Project/Manager/UI/Term.pm
+++ lib/Project/Manager/UI/Term.pm
@@ -3,8 +3,24 @@ package Project::Manager::UI::Term;
 use strict;
 use warnings;
 
+use Moo;
+use Tickit;
+use Tickit::Widget::Box;
+use Tickit::Widget::Static;
+
 sub run {
-	...
+	my $box = Tickit::Widget::Box->new(
+		h_border => 4,
+		v_border => 2,
+		bg       => "green",
+		child    => Tickit::Widget::Static->new(
+			text     => "Hello, world!",
+			bg       => "black",
+			align    => "centre",
+			valign   => "middle",
+		),
+	);
+	Tickit->new( root => $box )->run;
 }
 
 1;
