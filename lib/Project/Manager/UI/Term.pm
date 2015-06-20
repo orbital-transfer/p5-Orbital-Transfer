06b3cab961cde4762a59e5482823cca825b6beec
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-06-20 12:58:43 -0500

    wip: create a menu

--- lib/Project/Manager/UI/Term.pm
+++ lib/Project/Manager/UI/Term.pm
@@ -7,20 +7,32 @@ use Moo;
 use Tickit;
 use Tickit::Widget::Box;
 use Tickit::Widget::Static;
+use Tickit::Widget::Table;
+use Tickit::Widget::Menu;
+
+has _tickit => ( is => 'lazy' ); # _build__tickit
+has _widget_menu => ( is => 'lazy' ); # _build__widget_menu;
 
 sub run {
-	my $box = Tickit::Widget::Box->new(
-		h_border => 4,
-		v_border => 2,
-		bg       => "green",
-		child    => Tickit::Widget::Static->new(
-			text     => "Hello, world!",
-			bg       => "black",
-			align    => "centre",
-			valign   => "middle",
-		),
-	);
-	Tickit->new( root => $box )->run;
+	my $self = __PACKAGE__->new;
+
+	my $menu = $self->_widget_menu;
+	$menu->popup( $self->_tickit->rootwin, 5, 5 );
+	$self->_tickit->run;
 }
+
+sub _build__widget_menu {
+	my ($self) = @_;
+	my $menu = Tickit::Widget::Menu->new(
+	   items => [
+	      Tickit::Widget::Menu::Item->new(
+		 name => "Exit",
+		 on_activate => sub { $self->_tickit->stop }
+	      ),
+	   ],
+	);
+}
+
+sub _build__tickit { Tickit->new; }
 
 1;
