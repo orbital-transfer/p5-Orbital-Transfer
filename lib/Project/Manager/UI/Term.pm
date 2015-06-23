3a4bc219e7f538243b44437137182ea3227b8b4b
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-06-23 13:53:54 -0500

    wip: menubar for configuration

--- lib/Project/Manager/UI/Term.pm
+++ lib/Project/Manager/UI/Term.pm
@@ -6,34 +6,42 @@ use warnings;
 use Moo;
 use Tickit;
 use Tickit::Widget::Box;
+use Tickit::Widget::VBox;
 use Tickit::Widget::Static;
 use Tickit::Widget::Table;
 use Tickit::Widget::Menu;
+use Tickit::Widget::Frame;
+use Tickit::Widget::MenuBar;
 
 has _tickit => ( is => 'lazy' ); # _build__tickit
-has _widget_menu => ( is => 'lazy' ); # _build__widget_menu;
+has _widget_menubar => ( is => 'lazy' ); # _build__widget_menubar
 
 sub run {
 	my $self = __PACKAGE__->new;
 
-	my $menu = $self->_widget_menu;
-	$menu->popup( $self->_tickit->rootwin, 5, 5 );
+	$self->add_menubar;
 	$self->_tickit->run;
 }
 
-sub _build__widget_menu {
+sub add_menubar {
 	my ($self) = @_;
-	my $menu = Tickit::Widget::Menu->new(
-	   items => [
-	      Tickit::Widget::Menu::Item->new(
-		 name => "Configure",
-		 on_activate => sub { $self->screen_configure }
-	      ),
-	      Tickit::Widget::Menu::Item->new(
-		 name => "Exit",
-		 on_activate => sub { $self->_tickit->stop }
-	      ),
-	   ],
+	my $vbox = Tickit::Widget::VBox->new;
+	$self->_tickit->set_root_widget( $vbox );
+	$vbox->add( $self->_widget_menubar );
+}
+
+sub _build__widget_menubar {
+	my ($self) = @_;
+	Tickit::Widget::MenuBar->new(
+		items => [
+			Tickit::Widget::Menu->new(
+				name => "File",
+				items => [
+					Tickit::Widget::Menu::Item->new( name => "Configure", on_activate => sub { $self->screen_configure } ),
+					Tickit::Widget::Menu::Item->new( name => "Exit", on_activate => sub { $self->_tickit->stop } ),
+				],
+			),
+		]
 	);
 }
 
@@ -41,7 +49,28 @@ sub _build__tickit { Tickit->new; }
 
 sub screen_configure {
 	my ($self) = @_;
-	my $conf_win = $self->_tickit->rootwin->make_sub;
+	my $conf_win = $self->_tickit->rootwin->make_sub(
+		10, 10,
+		15,
+		15,
+		);
+	my $frame = Tickit::Widget::Frame->new;
+	$frame->set_window( $conf_win );
+
+	my $menu = Tickit::Widget::Menu->new(
+		items => [
+			Tickit::Widget::Menu::Item->new(
+				name => "Add account",
+				on_activate => sub { ... }
+			),
+			Tickit::Widget::Menu::Item->new(
+				name => "Back",
+				on_activate => sub { $conf_win->close }
+			),
+		],
+	);
+	$frame->add( $menu );
+	$conf_win->show;
 }
 
 1;
