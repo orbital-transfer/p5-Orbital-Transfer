796448fcd0214d4ed517848caf874e588c86e061
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-06-28 07:10:51 -0500

    make the repo listing request for GitHub an authenticated request
    This uses the GitHub token to retrieve all repos for the user.

--- lib/Project/Manager/Platform/GitHub/User.pm
+++ lib/Project/Manager/Platform/GitHub/User.pm
@@ -10,7 +10,16 @@ has user => ( is => 'ro', required => 1 );
 
 sub repos {
 	my ($self) = @_;
-	return $self->_pithub_client->repos->list( user => $self );
+	return $self->_pithub_client
+		->repos(
+			token => $self->github_token, # TODO what if this user does not have a token?
+			auto_pagination => 1,
+			)
+		->list(
+			#user => $self->user, # using authenticated
+			params => { type => 'all' },
+			options => $self->_pithub_options,
+			);
 }
 
 1;
