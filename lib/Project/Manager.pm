d301238c7b2a33176c33737e4dd731d69e6c672a
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-06-28 07:11:46 -0500

    build the DI container for retrieving the authenticated user

--- lib/Project/Manager.pm
+++ lib/Project/Manager.pm
@@ -5,6 +5,7 @@ use warnings;
 
 use Project::Manager::UI::Term;
 use Bread::Board;
+use Project::Manager::Platform::GitHub::User;
 
 sub run {
 	my ($self) = @_;
@@ -23,7 +24,20 @@ sub run {
 }
 
 sub app_container {
-	...
+	my $c = container ProjectManager => as {
+		service github_token => Project::Manager::Platform::GitHub->github_token;
+
+		service github_user => (
+			block => sub {
+				my $s = shift;
+				my $gh = Project::Manager::Platform::GitHub::User->new(
+					github_token => $s->param('github_token'),
+					);
+				$gh;
+			},
+			dependencies => [ 'github_token' ],
+		);
+	};
 }
 
 1;
