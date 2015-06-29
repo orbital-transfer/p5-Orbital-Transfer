137936b2e8a5ebb2ad22c0c58c80c85c4f5fd5ac
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-06-29 18:35:04 -0500

    resolve the github user in the web app

--- lib/Project/Manager/UI/Mojo.pm
+++ lib/Project/Manager/UI/Mojo.pm
@@ -7,6 +7,7 @@ with qw(Project::Manager::UI::Role::DI);
 # Route with placeholder
 get '/' => sub {
 	my $c = shift;
+	my $gh_repo = $c->container->resolve('github_user');
 	$c->render(text => "Hello from @{[ __PACKAGE__ ]}.");
 };
 
