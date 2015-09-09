a35917d9ae8be8e860243c36013dfc994909f6af
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-09-09 09:34:01 -0500

    add a list of Coveralls TODO tasks

--- lib/Project/Manager/Platform/Coveralls.pm
+++ lib/Project/Manager/Platform/Coveralls.pm
@@ -3,5 +3,12 @@ package Project::Manager::Platform::Coveralls;
 use Moo;
 
 # TODO
+# - login via GitHub
+# - retrieve a list of repositories
+# - add a repository from GitHub name
+# - list setting for repository
+# - get a list of builds (timestamp, commit SHA)
+# - get the coverage for a build
+# - get the coverage of each file in the build
 
 1;
