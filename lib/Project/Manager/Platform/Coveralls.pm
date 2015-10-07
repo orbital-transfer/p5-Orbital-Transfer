24b4621f5accea4e6ea87a305108773689072991
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-10-07 13:19:52 -0500

    Coveralls: move the extraction code to package

--- lib/Project/Manager/Platform/Coveralls.pm
+++ lib/Project/Manager/Platform/Coveralls.pm
@@ -1,14 +1,19 @@
 package Project::Manager::Platform::Coveralls;
 
 use Moo;
+
 use LWP::UserAgent;
 use HTTP::CookieJar;
 use HTML::Form;
+use HTML::TreeBuilder::XPath;
+use URI;
+
+use Types::Standard qw(Str InstanceOf);
 
 has coveralls_domain => ( is => 'rw',
-	default => sub {'https://coveralls.io/'} );
+	default => sub {URI->new('https://coveralls.io/')} );
 
-has ua => ( is => 'lazy' );
+has ua => ( is => 'lazy', isa => InstanceOf["LWP::UserAgent"] );
 
 sub _build_ua {
 	my $ua = LWP::UserAgent->new(
@@ -59,8 +64,50 @@ sub auth_to_bitbucket {
 	my $auth_url = 'https://coveralls.io/authorize/bitbucket'
 }
 
+# Contains the HTML of the base page for Coveralls (once logged in).
+#
+# builder: _build__base_html_content
+has _base_html_content => ( is => 'lazy', isa => Str );
+	sub _build__base_html_content {
+		my ($self) = @_;
+		return $self->ua->get($self->coveralls_domain)->decoded_content;
+	}
+
+# Contains an HTML::Tree of _base_html_content() with XPath support.
+#
+# Builder: _build__base_tree
+has _base_tree => ( is => 'lazy', isa => InstanceOf["HTML::TreeBuilder::XPath"] );
+	sub _build__base_tree {
+		my ($self) = @_;
+		return HTML::TreeBuilder::XPath->new_from_content(
+			$self->_base_html_content,
+		);
+	}
+
 sub repos {
-	...
+	my ($self) = @_;
+
+	my $coveralls_base = $self->coveralls_domain;
+	my $coveralls_tree = $self->_base_tree;
+	my @repos = $coveralls_tree->findnodes( q|//div[@class='repoOverview']| );
+	#use DDP; p $repos[0]->as_HTML;#DEBUG
+	my @repos_data = map {
+		my ($coverage_text_node) = $_->findnodes('.//div[contains(@class,"coverageText")]');
+		my ($coveralls_org_node, $coveralls_repo_node) = $_->findnodes('.//h1/a');
+		my $coverage_text =
+		+{
+			( $coverage_text_node ) ? (coverage =>  $coverage_text_node->as_trimmed_text) : (),
+			coveralls_organisation => {
+				name => $coveralls_org_node->as_trimmed_text,
+				link => URI->new_abs($coveralls_org_node->attr('href'), $coveralls_base),
+			},
+			coveralls_repo => {
+				name => $coveralls_repo_node->as_trimmed_text,
+				link => URI->new_abs($coveralls_repo_node->attr('href'), $coveralls_base),
+			},
+			text => $_->as_trimmed_text,
+		}
+	} @repos;
 	# - get the link to the corresponding GitHub repo
 	# - turn that into a GitHub repo object
 	# - get the coverage for the project
