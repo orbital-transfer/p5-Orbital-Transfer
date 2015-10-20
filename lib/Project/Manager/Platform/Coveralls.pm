aeb7c6184428798397980f74eeadc2213bb346c1
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-10-19 23:10:15 -0500

    Coveralls: move the extraction into Coveralls::Repo object

--- lib/Project/Manager/Platform/Coveralls.pm
+++ lib/Project/Manager/Platform/Coveralls.pm
@@ -8,7 +8,6 @@ use HTML::Form;
 use HTML::TreeBuilder::XPath;
 use URI;
 use HTML::FormatText;
-use String::Strip qw(StripLTSpace);
 
 use Types::Standard qw(Str InstanceOf);
 use Project::Manager::Platform::Coveralls::Repo;
@@ -91,87 +90,23 @@ has _base_tree => ( is => 'lazy', isa => InstanceOf["HTML::TreeBuilder::XPath"] 
 sub repos {
 	my ($self) = @_;
 
-	my $fmt_text = HTML::FormatText->new;
 	my $coveralls_base = $self->coveralls_domain;
 	my $coveralls_tree = $self->_base_tree;
 	my @repos = $coveralls_tree->findnodes( q|//div[@class='repoOverview']| );
-	#use DDP; p $repos[0]->as_HTML;#DEBUG
 
 	my @repos_data = map {
-		my ($coverage_text_node) = $_->findnodes('.//div[contains(@class,"coverageText")]');
-		my ($coveralls_org_node, $coveralls_repo_node) = $_->findnodes('.//h1/a');
-		my ($build_summary) = $_->findnodes('.//div[contains(@class,"summary")]');
-		my ($build_number, $build_details) = $build_summary->findnodes('./a');
-
-
-		my $repo_data = +{
-			_test =>
-			do {
-				my $r = Project::Manager::Platform::Coveralls::Repo->new(
-						coveralls_domain => $coveralls_base,
-						repo_overview_node => $_
-					       );
-				my $attrs = 'Moo'->_constructor_maker_for('Project::Manager::Platform::Coveralls::Repo')->all_attribute_specs;
-				for my $attr (keys %$attrs) {
-					if( exists $attrs->{$attr}{lazy} ) {
-						$r->$attr();
-					}
-				}
-				$r
-			},
-			( $coverage_text_node ) ? (coverage =>  $coverage_text_node->as_trimmed_text) : (),
-			build => {
-				text => do {
-					my $bs_text = $fmt_text->format($build_summary);
-					StripLTSpace($bs_text);
-					$bs_text;
-				},
-
-				  ($build_number)
-				? (last_build => {
-						number => ( $build_number->as_trimmed_text =~ /Build #(?<build>\d+)/ )[0] ,
-						link => URI->new_abs($build_number->attr('href'), $coveralls_base),
-						details => do {
-							#my $bd_text = $fmt_text->format($build_details);
-							my $bd_text = $build_details->as_trimmed_text;
-							StripLTSpace($bd_text);
-							$bd_text;
-						},
-						branch => do {
-							my ($branch) = $build_summary->findnodes('./strong');
-							$branch->as_text;
-						},
-					})
-				: (),
-			},
-			coveralls_org => {
-				name => $coveralls_org_node->as_trimmed_text,
-				link => URI->new_abs($coveralls_org_node->attr('href'), $coveralls_base),
-			},
-			coveralls_repo => {
-				name => $coveralls_repo_node->as_trimmed_text,
-				link => URI->new_abs($coveralls_repo_node->attr('href'), $coveralls_base),
-			},
-			text => do {
-				my $text = $fmt_text->format($_);
-				StripLTSpace($text);
-				$text;
-			},
-		};
-
-		# get the link to the corresponding GitHub repo
-		my $github_repo_uri = URI->new('https://github.com');
-		$github_repo_uri->path_segments(
-			$repo_data->{coveralls_org}{name},
-			$repo_data->{coveralls_repo}{name},
-			);
-		$repo_data->{github_repo}{link} = $github_repo_uri;
-
-		$repo_data;
+		my $r = Project::Manager::Platform::Coveralls::Repo->new(
+				coveralls_domain => $coveralls_base,
+				repo_overview_node => $_
+			       );
+		my $attrs = 'Moo'->_constructor_maker_for('Project::Manager::Platform::Coveralls::Repo')->all_attribute_specs;
+		for my $attr (keys %$attrs) {
+			if( exists $attrs->{$attr}{lazy} ) {
+				$r->$attr();
+			}
+		}
+		$r
 	} @repos;
-	# - turn the github repo link into a GitHub repo object
-	# - get the coverage for the project
-	# - time of last coverage build
 }
 
 # TODO
--- lib/Project/Manager/Platform/Coveralls/Repo.pm
+++ lib/Project/Manager/Platform/Coveralls/Repo.pm
@@ -1,10 +1,15 @@
 package Project::Manager::Platform::Coveralls::Repo;
 
+# TODO
+# - get the coverage for the project
+# - time of last coverage build
+
 use strict;
 use warnings;
 
 use Moo;
 use MooX::HandlesVia;
+use String::Strip qw(StripLTSpace);
 
 has coveralls_domain => ( is => 'ro', required => 1 );
 has repo_overview_node => ( is => 'rw' );
@@ -13,6 +18,12 @@ has _node_coverage_text => ( is => 'lazy');
 	sub _build__node_coverage_text {
 		my ($self) = @_;
 		my ($coverage_text_node) = $self->repo_overview_node->findnodes('.//div[contains(@class,"coverageText")]');
+		$coverage_text_node;
+	}
+has coverage_text => ( is => 'lazy' );
+	sub _build_coverage_text {
+		my ($self) = @_;
+		( $self->_node_coverage_text ) ? ( $self->_node_coverage_text->as_trimmed_text ) : (),
 	}
 
 has _node_coverage_head_links => (
@@ -28,17 +39,97 @@ has _node_coverage_head_links => (
 		[ my ($coveralls_org_node, $coveralls_repo_node) = $self->repo_overview_node->findnodes('.//h1/a') ];
 	}
 
+has _node_build_summary => ( is => 'lazy',);
+	sub _build__node_build_summary {
+		my ($self) = @_;
+		my ($build_summary) = $self->repo_overview_node->findnodes('.//div[contains(@class,"summary")]');
+		$build_summary;
+	}
+
+has _node_build_summary_details => (
+	is => 'lazy',
+	handles_via => 'Array',
+	handles => {
+		_node_build_number  => [ get => 0 ],
+		_node_build_details => [ get => 1 ],
+	},
+);
+	sub _build__node_build_summary_details {
+		my ($self) = @_;
+		[ my ($build_number, $build_details) = $self->_node_build_summary->findnodes('./a') ];
+	}
+
 has _text_formatter => ( is => 'lazy' );
 	sub _build__text_formatter {
 		my $fmt_text = HTML::FormatText->new;
 	}
 
 has build_text => ( is => 'lazy' );
-sub _build_build_text {
+	sub _build_build_text {
+		my ($self) = @_;
+		my $bs_text = $self->_text_formatter->format($self->_node_build_summary);
+		StripLTSpace($bs_text);
+		$bs_text;
+	}
 
-}
+has last_build => (
+	is => 'lazy',
+	handles_via => 'Hash',
+	handles => {
+		last_build_number  => [ get => 'number'  ],
+		last_build_link    => [ get => 'link'    ],
+		last_build_details => [ get => 'details' ],
+		last_build_branch  => [ get => 'branch'  ],
+	},
+);
+	sub _build_last_build {
+		my ($self) = @_;
+		  ($self->_node_build_number)
+		? ({
+				number => ( $self->_node_build_number->as_trimmed_text =~ /Build #(?<build>\d+)/ )[0] ,
+				link => URI->new_abs($self->_node_build_number->attr('href'), $self->coveralls_domain),
+				details => do {
+					my $bd_text = $self->_node_build_details->as_trimmed_text;
+					StripLTSpace($bd_text);
+					$bd_text;
+				},
+				branch => do {
+					my ($branch) = $self->_node_build_summary->findnodes('./strong');
+					$branch->as_text;
+				},
+		  })
+		: (),
+	}
 
+has _coveralls_metadata => (
+	is => 'lazy',
+	handles_via => 'Hash',
+	handles => {
+		org_name  =>  [ get => 'org_name'  ],
+		org_link  =>  [ get => 'org_link'    ],
+		repo_name =>  [ get => 'repo_name' ],
+		repo_link  => [ get => 'repo_link'  ],
+	},
+);
+	sub _build__coveralls_metadata {
+		my ($self) = @_;
+		+{
+			org_name => $self->_node_coverage_org->as_trimmed_text,
+			org_link => URI->new_abs($self->_node_coverage_org->attr('href'), $self->coveralls_domain),
+			repo_name => $self->_node_coverage_repo->as_trimmed_text,
+			repo_link => URI->new_abs($self->_node_coverage_repo->attr('href'), $self->coveralls_domain),
+		},
+	}
 
+has github => ( is => 'lazy',);
+	sub _build_github {
+		my ($self) = @_;
+		require Project::Manager::Platform::GitHub::Repo;
+		Project::Manager::Platform::GitHub::Repo->new(
+			namespace => $self->org_name,
+			name => $self->repo_name,
+		);
+	}
 
 
 1;
