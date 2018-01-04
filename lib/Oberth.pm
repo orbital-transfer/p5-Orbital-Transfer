ba92b38adbeb33dc4230ca98ea8582fa5372c189
Zakariyya Mughal <zaki.mughal@gmail.com>
2018-01-03 19:44:01 -0600

    Remove CLI from repo

--- lib/Oberth.pm
+++ lib/Oberth.pm
@@ -1,15 +1,5 @@
 use Oberth::Common::Setup;
 package Oberth;
-# ABSTRACT: Run Oberth
-
-use Moo;
-use CLI::Osprey;
-
-subcommand config => 'Oberth::Command::Config';
-subcommand list => 'Oberth::Command::List';
-
-method run(@) {
-	...
-}
+# ABSTRACT: Software project manager
 
 1;
