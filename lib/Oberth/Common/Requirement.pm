7525f280a47aaf47aa0631bbc0afb22d346b20e4
Zakariyya Mughal <zaki.mughal@gmail.com>
2018-09-20 17:54:25 -0500

    Add requirement for updated version of Moose

---
+++ lib/Oberth/Common/Requirement.pm
@@ -0,0 +1,8 @@
+use Modern::Perl;
+package Oberth::Common::Requirement;
+# ABSTRACT: List of requirements for distribution
+
+# minimum version of Moose needed for configuring Function::Parameters
+use Moose 2.2005 ();
+
+1;
