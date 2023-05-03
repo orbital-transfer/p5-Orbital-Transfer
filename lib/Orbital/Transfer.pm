f4d666782d11873b711fdc7a6b0ef76654890cb0
Zakariyya Mughal <zaki.mughal@gmail.com>
2023-05-02 23:40:37 -0400

    Start of plugin system

--- lib/Orbital/Transfer.pm
+++ lib/Orbital/Transfer.pm
@@ -2,4 +2,9 @@ use Orbital::Transfer::Common::Setup;
 package Orbital::Transfer;
 # ABSTRACT: Software project manager
 
+use strict;
+use warnings;
+
+use Module::Pluggable require => 1, search_path => [ 'Orbital::Payload::Container' ], sub_name => 'containers';
+
 1;
