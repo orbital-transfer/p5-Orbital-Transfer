12895986d641aa44f99a49d60bfa8f601d84c219
Zakariyya Mughal <zaki.mughal@gmail.com>
2023-05-04 08:47:48 -0400

    Add builder plugin

--- lib/Orbital/Transfer.pm
+++ lib/Orbital/Transfer.pm
@@ -8,9 +8,17 @@ use warnings;
 use Module::Pluggable require => 1, search_path => [ 'Orbital::Payload::Container' ], sub_name => 'containers';
 
 classmethod finders(:$containers = undef) {
+	$class->_plugin_by_type('finder', containers => $containers);
+}
+
+classmethod builders(:$containers = undef) {
+	$class->_plugin_by_type('builder', containers => $containers);
+}
+
+classmethod _plugin_by_type($type, :$containers = undef) {
 	map {
 		my $init = $_->initialize;
-		exists $init->{finder} ? $init->{finder}->@* : ()
+		exists $init->{$type} ? $init->{$type}->@* : ()
 	} defined $containers ? @$containers : $class->containers;
 }
 
